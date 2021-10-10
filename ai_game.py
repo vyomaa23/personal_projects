e38a9182f4bc1f6b59704182f0d2e8278105025f
cameo11 <sainiabhijeet11@gmail.com>
2021-10-10 18:33:30 -0500

    feature main - dev1

---
+++ ai_game.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+class AlienInvasion:
+	""" Class manage game assests and behavior"""
+
+	def __init__(self):
+		pygame.init()
+
+		self.screen = pygame.display.set_mode((1200, 800))
+		pygame.display.set_caption("Alien Invasion")
+
+	def run_game(self):
+		"""start the main loop"""
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+			pygame.display.flip()
+
+
+if __name__ = '__main__':
+	ai = AlienInvasion()
+	ai.run_game()
